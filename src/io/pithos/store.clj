acd92101a1fce5a434bd24b14592049b99050327
Sergei Antipov <greendayonfire@gmail.com>
2019-10-29 13:38:38 -0400

    Typo

--- src/io/pithos/store.clj
+++ src/io/pithos/store.clj
@@ -25,7 +25,7 @@
         cluster (if (sequential? cluster) cluster [cluster])
         tls (or tls false)
         tls-options (or tls-options
-                        ;; List of Mozilla's recommended suites for TLSv1.2(maximum support in Java 8)
+                        ;; List of Mozilla's recommended suites for TLSv1.2(maximum supported in Java 8)
                         {:cipher-suites ["TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256" "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256" "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384" "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384" "TLS_DHE_RSA_WITH_AES_128_GCM_SHA256" "TLS_DHE_RSA_WITH_AES_256_GCM_SHA384"]
                          :keystore-path (str (. (. System getProperties) (get "java.home")) "/lib/security/cacerts")
                          :keystore-password "changeit"})
