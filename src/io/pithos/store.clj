c47c5dcee8c6b52b5d7b6bc997685e1ede19d25a
Sergei Antipov <greendayonfire@gmail.com>
2019-10-29 16:00:53 -0400

    Platform-neutral implementation of :keystore-path

--- src/io/pithos/store.clj
+++ src/io/pithos/store.clj
@@ -3,7 +3,8 @@
   (:import com.datastax.driver.core.exceptions.InvalidQueryException)
   (:require [qbits.alia            :as alia]
             [qbits.hayt            :refer [use-keyspace create-keyspace with]]
-            [clojure.tools.logging :refer [debug]]))
+            [clojure.tools.logging :refer [debug]]
+            [clojure.java.io       :as io]))
 
 (defprotocol Convergeable
   (converge! [this]))
@@ -27,7 +28,7 @@
         tls-options (or tls-options
                         ;; List of Mozilla's recommended suites for TLSv1.2(maximum supported in Java 8)
                         {:cipher-suites ["TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256" "TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256" "TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384" "TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384" "TLS_DHE_RSA_WITH_AES_128_GCM_SHA256" "TLS_DHE_RSA_WITH_AES_256_GCM_SHA384"]
-                         :keystore-path (str (. (. System getProperties) (get "java.home")) "/lib/security/cacerts")
+                         :keystore-path (-> (io/file (. (. System getProperties) (get "java.home")) "lib" "security" "cacerts") (.getPath))
                          :keystore-password "changeit"})
         session (-> (assoc cassandra-options :contact-points cluster)
                     (cond-> (and username password)
